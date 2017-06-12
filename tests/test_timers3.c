827971df0ffa37636350ef5d437a3ebab11e8c4f
Daniele Lacamera <dan@danielinux.net>
2017-06-12 10:38:45 +0200

    Added two timer tests

---
+++ tests/test_timers3.c
@@ -0,0 +1,29 @@
+#include "libevquick.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int counter = 0;
+void timer_cb(void *arg)
+{
+    counter++;
+    evquick_addtimer(5, 0, timer_cb, NULL);
+}
+
+void stats(void *arg)
+{
+    printf("Counter: %d\n", counter);
+    counter = 0;
+}
+
+int main(void)
+{
+	if(evquick_init() < 0)
+		exit(2);
+
+    evquick_addtimer(5, 0, timer_cb, NULL);
+    evquick_addtimer(1000, EVQUICK_EV_RETRIGGER, stats, NULL);
+
+	evquick_loop();
+	exit(0);
+}
