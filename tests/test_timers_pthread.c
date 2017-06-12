1352fd95d675c835515f6c2e97bec006a9cd8c72
Daniele Lacamera <root@danielinux.net>
2017-06-12 21:15:20 +0200

    Multi-thread timer test changed to reproduce the hang

--- tests/test_timers_pthread.c
+++ tests/test_timers_pthread.c
@@ -5,18 +5,28 @@
 #include <pthread.h>
 
 static int counter = 0;
+static int counter1 = 0;
 
 static __thread CTX ctx = NULL;
+
+void timer_long_cb(CTX c, void *arg)
+{
+    counter1++;
+    evquick_addtimer(ctx, 200, 0, timer_long_cb, NULL);
+}
 
 void timer_cb(CTX c, void *arg)
 {
     counter++;
 }
 
+
 void timer2_cb(CTX c, void *arg)
 {
-    printf("Counter: %d\n", counter);
+    printf("Counters: %d %d\n", counter, counter1);
     counter = 0;
+    counter1 = 0;
+    evquick_addtimer(ctx, 1000, 0, timer2_cb, NULL);
 }
 
 void *thread2(void *arg)
@@ -24,10 +34,9 @@ void *thread2(void *arg)
 	ctx = evquick_init();
     if (!ctx)
 		exit(2);
-    evquick_addtimer(ctx, 1000, EVQUICK_EV_RETRIGGER, timer2_cb, NULL);
+    evquick_addtimer(ctx, 1000, 0, timer2_cb, NULL);
 	evquick_loop(ctx);
 }
-
 
 int main(void)
 {
@@ -37,6 +46,7 @@ int main(void)
     if (!ctx)
 		exit(2);
     evquick_addtimer(ctx, 8, EVQUICK_EV_RETRIGGER, timer_cb, NULL);
+    evquick_addtimer(ctx, 200, 0, timer_long_cb, NULL);
 	evquick_loop(ctx);
 	exit(0);
 }
